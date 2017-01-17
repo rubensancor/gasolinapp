2cc14b26a8c6194ae654aa10654795a7024313d9
gusy <rubensancor@opendeusto.es>
2017-01-17 12:47:59 +0100

    a

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ function read() {
 
 
 //El servidor se pone a escuchar en el puerto asignado y escribe por consola
-app.listen(serverPort, function() {
+app.listen((process.env.PORT || 5000), function() {
 /*    PythonShell.run('parser_gasolineras.py', function() {
 
         MongoClient.connect("mongodb://gusy:das@ds155747.mlab.com:55747/heroku_50hc8zr3", function(err, db) {
